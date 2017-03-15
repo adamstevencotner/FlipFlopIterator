99a37185c31687e93258bf3f9e7b1245b5bf852e
AdamCotner <adamstevencotner@gmail.com>
2017-03-14 19:27:08 -0600

    project to git

---
+++ FlipFlopIterator/FlipFlopIterator.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+
+public class FlipFlopIterator
+{
+
+    public FlipFlopIterator(int _startIndex, int _lowerBound, int _upperBound)
+    {
+        index = _startIndex;
+        lowerBound = _lowerBound;
+        upperBound = _upperBound;
+
+        init = _startIndex;
+        diff = 0;
+        add = true;
+        lockOp = false;
+
+        // throw if invalid here
+    }
+
+    // the algoritm
+    public int? Get()
+    {
+        // set index immediately
+        index = init + diff;
+
+        // if we've overstepped our bounds, course correct
+        // and lock the operation
+        if (index < lowerBound || index > upperBound)
+        {
+            // if we've set the lock, return null. we're done here.
+            // i suppose you could also throw an exception, or
+            // really anything that indicates that we're done
+            if (lockOp)
+                return null;
+
+            // if we just added, check the opposite
+            // if we just negated, check opposite + 1
+            diff = !add ? -1 * diff : (-1 * diff) + 1;
+                
+            // set lock to only increment or decrement
+            lockOp = true;
+
+            // Advance to Go. Collect $200.
+            return Get();
+        }
+
+
+        if (lockOp)
+        {
+            // just add or subtract, no more flip flopping
+            diff = add ? diff + 1 : diff - 1;
+        }
+        else
+        {
+            // flip the sign and add one if add is true
+            // also change direction for next time
+            diff = add ? (-1 * diff) + 1 : -1 * diff;
+            add = !add;
+        }
+
+        return index;
+    }
+
+    public void Reset()
+    {
+        index = init;
+        diff = 0;
+        add = true;
+        lockOp = false;
+    }
+
+    protected int? index { get; set; }
+    protected int lowerBound { get; set; }
+    protected int upperBound { get; set; }
+
+    protected readonly int init;
+    protected int diff { get; set; }
+    protected bool add { get; set; }
+    protected bool lockOp { get; set; }
+}
+
+// same same, but for iterating types
+public class FlipFlopIterator<T> : FlipFlopIterator
+{
+    public FlipFlopIterator(IList<T> _collection, T _startElement)
+        : base (_collection.IndexOf(_startElement), 0, _collection.Count)
+    {
+        collection = _collection;
+    }
+        
+    // returns the object at an index, rather than an index
+    public IEnumerable<T> Iterate()
+    {
+        for (var i = 0; i < collection.Count; i++)
+        {
+            var index = Get();
+
+            // this shouldnt happen because we're looping over
+            // count, but just in case
+            if (index == null) break;
+
+            yield return collection[(int) index];
+        }
+    }
+
+    private readonly IList<T> collection;
+}
---
+++ FlipFlopIteratorTests/FlipFlopIteratorTests.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+[TestFixture]
+public class FlipFlopIteratorTests
+{
+    private FlipFlopIterator iterator;
+    private FlipFlopIterator<object> typedIterator;
+
+    [SetUp]
+    public void SetUp()
+    {
+    }
+
+    private List<int?> GetIterationList()
+    {
+        var result = new List<int?>();
+        int? tmp;
+        int loop = 0;
+
+        while (true)
+        {
+            tmp = iterator.Get();
+            Console.WriteLine(tmp);
+            // leave if we're done
+            if (tmp == null)
+                break;
+
+            result.Add(tmp);
+
+            // delete this.
+            if (++loop > 7) throw new Exception("infinite loop detected");
+        }
+
+        return result;
+    }
+
+    private List<object> GetTypedIterationList()
+    {
+        var result = new List<object>();
+        int loop = 0;
+
+        foreach (var obj in typedIterator.Iterate())
+        {
+            // cant happen, but whatever
+            if (obj == null)
+                break;
+
+            result.Add(obj);
+
+            // delete this.
+            if (++loop > 7) throw new Exception("infinite loop detected");
+        }
+
+        return result;
+    }
+
+    [Test]
+    public void FlipFlopIteratorTest_1()
+    {
+        iterator = new FlipFlopIterator(3, 1, 5);
+
+        var actual = GetIterationList();
+        var expected = new List<int?> { 3, 4, 2, 5, 1 };
+
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+
+    [Test]
+    public void FlipFlopIteratorTest_2()
+    {
+        iterator = new FlipFlopIterator(3, 1, 6);
+
+        var actual = GetIterationList();
+        var expected = new List<int?> { 3, 4, 2, 5, 1, 6 };
+
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+
+    [Test]
+    public void FlipFlopIteratorTest_3()
+    {
+        iterator = new FlipFlopIterator(-1, -4, 1);
+
+        var actual = GetIterationList();
+        var expected = new List<int?> { -1, 0, -2, 1, -3, -4 };
+
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+
+    [Test]
+    public void FlipFlopIteratorTest_4()
+    {
+        iterator = new FlipFlopIterator(0, 0, 0);
+
+        var actual = GetIterationList();
+        var expected = new List<int?> { 0 };
+
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+
+    [Test]
+    public void FlipFlopIteratorTest_5()
+    {
+        iterator = new FlipFlopIterator(-4, -4, 1);
+
+        var actual = GetIterationList();
+        var expected = new List<int?> { -4, -3, -2, -1, 0, 1 };
+
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+
+    [Test]
+    public void FlipFlopIteratorTest_6()
+    {
+        iterator = new FlipFlopIterator(1, -4, 1);
+
+        var actual = GetIterationList();
+        var expected = new List<int?> { 1, 0, -1, -2, -3, -4 };
+
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+
+    [Test]
+    public void FlipFlopIteratorTest_7()
+    {
+        iterator = new FlipFlopIterator(-3, -6, -1);
+
+        var actual = GetIterationList();
+        var expected = new List<int?> { -3, -2, -4, -1, -5, -6 };
+
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+
+    [Test]
+    public void FlipFlopIteratorTest_8()
+    {
+        iterator = new FlipFlopIterator(-4, -6, 0);
+
+        var actual = GetIterationList();
+        var expected = new List<int?> { -4, -3, -5, -2, -6, -1, 0 };
+
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+
+    [Test]
+    public void FlipFlopIteratorTest_9()
+    {
+        iterator = new FlipFlopIterator(-4, -6, -1);
+
+        var actual = GetIterationList();
+        var expected = new List<int?> { -4, -3, -5, -2, -6, -1 };
+
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+
+    [Test]
+    public void FlipFlopIteratorTest_10()
+    {
+        var item1 = new { val = "Item1" };
+        var item2 = new { val = "Item2" };
+        var item3 = new { val = "Item3" };
+        var item4 = new { val = "Item4" };
+        var item5 = new { val = "Item5" };
+
+        var collection = new List<object> { item1, item2, item3, item4, item5 };
+        typedIterator = new FlipFlopIterator<object>(collection, item2);
+
+        var actual = GetTypedIterationList();
+        var expected = new List<object> { item2, item3, item1, item4, item5 };
+
+        Assert.That(actual, Is.EquivalentTo(expected));
+    }
+}
